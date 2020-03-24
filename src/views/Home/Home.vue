a95ee8c45c03b7fa3d2b8a35476cdfbb554f6afe
wangxinleo <wangxin.leo@outlook.com>
2020-03-24 17:25:21 +0800

    主页导航栏

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,13 +1,35 @@
 <template>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
+  import NavBar from "../../components/common/navbar/NavBar";
+  import {getHomeMultidata} from "../../network/home"
     export default {
-        name: "Home"
+      name: "Home",
+      components:{
+        NavBar
+      },
+      data(){
+        return {
+          banners: [],
+          recommend: []
+        }
+      },
+      created() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommend = res.data.recommend.list;
+        })
+      }
     }
 </script>
 
 <style scoped>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
